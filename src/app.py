9366bbf0fc9c4760b3b4b44af022af3becf59fc6
sapk-pa1 <sapkotapawan099@gmail.com>
2023-07-10 17:07:39 +0545

    basic render of HTML

--- src/app.py
+++ src/app.py
@@ -1,12 +1,16 @@
 
 from flask import Flask, render_template
 
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
+@app.route("/", methods = ['GET'])
+def home():
     return render_template("index.html")
 
+@app.route("/predict", methods=['GET', 'POST'])
+def predict():
+    pass
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug =True)
